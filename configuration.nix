6e077fdaf1f77865980174c65ac9a3fd296c73a1
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-03-02 14:01:21 -0500

    47 current  2024-03-02 13:49:37  23.11.4874.068d4db60495  6.1.79                          *

--- configuration.nix
+++ configuration.nix
@@ -184,6 +184,7 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
+
   environment.interactiveShellInit = ''
     alias rebuild="bash ~/Documents/Scripts/nixos-rebuild.sh"
   '';
