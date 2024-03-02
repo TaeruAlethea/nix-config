6bf1e4c2cc296f2cacf7b5ea018f9dfd53786e4c
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-03-02 01:22:09 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -164,6 +164,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    alejandra
     bottles
     easyeffects
     git
