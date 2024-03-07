e750b8c1edb8f1a687030cb175a86f41539a2ebe
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-03-06 20:18:42 -0500

    74 current  2024-03-06 19:48:04  24.05pre591063.b8697e57f102  6.6.19                          *

--- home.nix
+++ home.nix
@@ -1,12 +1,19 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   home = {
-    packages = [pkgs.atool pkgs.httpie];
+    username = "taeru";
+    homeDirectory = "/home/taeru";
 
     sessionVariables = {
       EDITOR = "vscode";
       BROWSER = "firefox";
       TERMINAL = "Kitty";
     };
+
+    packages = [pkgs.atool pkgs.httpie];
   };
 
   programs.bash.enable = true;
@@ -20,4 +27,7 @@
   # The state version is required and should stay at the version you
   # originally installed.
   home.stateVersion = "24.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
