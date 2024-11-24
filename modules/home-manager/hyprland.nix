08690f2c9016380d3adadfd8cad95a132023d364
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-11-24 11:38:41 -0500

    Update hyprland.nix

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -132,55 +132,63 @@ in
     general = { 
       disable_loading_bar = true;
     }
-    background = {
-      monitor=eDP-1
-      blur_passes=1
-      noise=0.011700
-      path=/tmp/screenshot1.png
-    }
-    image {
-      monitor=eDP-1
-      halign=center
-      path=/home/astraeaf/Pictures/profile.png
-      position=0, 50
-      valign=center
-    }
-    input-field {
-      monitor=eDP-1
-      size=200,50
-      check_color=rgb(30, 107, 204)
-      dots_center=true
-      dots_size=0.200000
-      fade_on_empty=false
-      font_color=rgb(111, 45, 104)
-      halign=center
-      inner_color=rgba(0, 0, 0, 0.2)
-      outer_color=rgba(0, 0, 0, 0)
-      outline_thickness=2
-      placeholder_text=<i><span foreground="##cdd6f4">Input Password...</span></i>
-      position=0, -100
-      rounding=-1
-      valign=center
-    }
-    label {
-      monitor=eDP-1
-      color=rgba(242, 243, 244, 0.75)
-      font_family=JetBrains Mono
-      font_size=95
-      halign=center
-      position=0, 300
-      time=$TIME
-      valign=center
-    }
-    label {
-      monitor=eDP-1
-      color=rgba(242, 243, 244, 0.75)
-      font_family=JetBrains Mono
-      font_size=22
-      halign=center
-      position=0,200
-      text=cmd[update:1000] echo $(date +"%A, %B %d")
-      valign=center
-    }
+    # inspiration from https://github.com/justinmdickey/publicdots/blob/main/.config/hypr/hyprlock.conf
+    background = [{
+      monitor = "eDP-1";
+      path = "/tmp/screenshot1.png";
+
+      blur_passes = 1; # 0 disables blurring
+      blur_size = 7;
+      noise = 1.17e-2;
+    }];
+    
+    label = [{
+      monitor = "eDP-1";
+      text = "$TIME";
+      color = "rgba(242, 243, 244, 0.75)";
+      font_size = 95;
+      font_family = "JetBrains Mono";
+      position = "0, 300";
+      halign = "center";
+      valign = "center";
+    }{
+      monitor = "eDP-1";
+      text = ''cmd[update:1000] echo $(date +"%A, %B %d")'';
+      color = "rgba(242, 243, 244, 0.75)";
+      font_size = 22;
+      font_family = "JetBrains Mono";
+      position = "0, 200";
+      halign = "center";
+      valign = "center";
+    }];
+
+    image = {
+      monitor = "eDP-1";
+      path = "/home/${username}/Pictures/profile.png";
+
+      position = "0, 50";
+      halign = "center";
+      valign = "center";
+    };
+
+    input-field = {
+      monitor = "DP-2";
+      size = "200,50";
+      outline_thickness = 2;
+      dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
+      dots_spacing = 0.35; # Scale of dots' absolute size, 0.0 - 1.0
+      dots_center = true;
+      outer_color = "rgba(0, 0, 0, 0)";
+      inner_color = "rgba(0, 0, 0, 0.2)";
+      font_color = "rgb(111, 45, 104)";
+      fade_on_empty = false;
+      rounding = -1;
+      check_color = "rgb(30, 107, 204)";
+      placeholder_text = ''<i><span foreground="##cdd6f4">Input Password...</span></i>'';
+      hide_input = false;
+      position = "0, -100";
+      halign = "center";
+      valign = "center";
+    };
   };
 }
