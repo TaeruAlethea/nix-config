7b60d9ed5c41d48266f8b9d154c6b71be1fa3dc7
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-11-25 12:47:26 -0500

    Update configuration.nix

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -154,28 +154,6 @@ in
 #    '';
 #    image = "/home/astraeaf/Pictures/wallpaper.jpg";
   };
-  home.pointerCursor = {
-    gtk.enable = true;
-    # x11.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
-    size = 16;
-  };
-  gtk = {
-    enable = true;
-    theme = {
-      package = pkgs.flat-remix-gtk;
-      name = "Flat-Remix-GTK-Grey-Darkest";
-    };
-    iconTheme = {
-      package = pkgs.gnome.adwaita-icon-theme;
-      name = "Adwaita";
-    };
-    font = {
-      name = "Sans";
-      size = 11;
-    };
-  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
