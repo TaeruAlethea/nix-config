71e7f876a2f95106c6dc1f0def6adcc497084e45
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-11-25 01:06:14 -0500

    tpyo 2

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -96,7 +96,7 @@
     validateSopsFiles = false;
 
     age = {
-      sshKeysPaths = [ "/etc/ssh/ssh_host_ed25519_key"];
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key"];
       keyFile = "/var/lib/sops-nix/key.txt";
       generateKey = true;
     };
