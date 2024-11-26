ab5ca58150927c37c26b6383d6ee2f00f77c4e8a
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-11-25 21:00:15 -0500

    Flake adaptation to EmergentMind style

--- flake.nix
+++ flake.nix
@@ -1,38 +1,74 @@
 {
-  description = "Nixos config flake";
+description = "TaeruAlethea's flake";
 
-  inputs = {
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+outputs = { 
+    self, 
+    nixpkgs, 
+    home-manager, 
+    stylix, 
+    ... 
+  }@inputs: 
+  let
+    inherit (self) outputs;
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      #"aarch64-darwin"
+    ];
+    inherit (nixpkgs) lib;
+    configVars = import ./vars { inherit inputs lib; };
+    configLib = import ./lib { inherit lib; };
+    specialArgs = {
+      inherit
+        inputs
+        outputs
+        configVars
+        configLib
+        nixpkgs
+        ;
+    };
+  in
+  {
+
+  # formatter, command through 'nix fmt' https://nix-community.github.io/nixpkgs-fmt
+  formatter = forAllSystems (system: nixpkgs.${system}.nixfmt-rfc-style);
+
+  # Configurations. Additional hosts should be added inside nixosConfigurations
+    nixosConfigurations = {
+      ares = lib.nixosSystem {
+        inharit specialArgs;
+        modules = [
+          { home-manager.extraSpecialArgs = specialArgs; }
+          ./hosts/ares
+        ];
+      };
+    };
+  };
+
+inputs = {
+  ### NixOS and HM Package Sources ###
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+  ### Utilities ###
+
+  # Secret Management
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
-
+  # Themeing
     stylix.url = "github:danth/stylix";
-  };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations = {
-      laptop = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/laptop/configuration.nix
-          inputs.nixos-hardware.nixosModules.microsoft-surface-common
-          inputs.sops-nix.nixosModules.sops
-          inputs.stylix.nixosModules.stylix
-        ];
-      };
+  # Vim. As good a time to learn it as any.
+    nixvim ={
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 }
