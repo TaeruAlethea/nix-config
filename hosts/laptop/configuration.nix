7d2db7cac1c6acf4df7246ef143a76c7210f19b2
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-11-27 10:15:28 -0500

    Nerdfonts Jetbrains overlay

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -184,7 +184,7 @@ in
   fonts = {
     packages = with pkgs; [
     # Fallback Fonts
-      jetbrains-mono
+      (nerdfonts.override { fonts = [ "JetBrainsMono"]; })
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
