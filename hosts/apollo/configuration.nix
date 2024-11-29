11ef7682e2708c5558209c795edf8b5794ccc210
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-11-29 18:21:55 -0500

    .

--- hosts/apollo/configuration.nix
+++ hosts/apollo/configuration.nix
@@ -70,7 +70,7 @@
 
     # Opinionated: make flake registry and nix path match flake inputs
     registry = lib.mkForce lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    nixPath = lib.mkForce lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
   networking.hostName = "apollo"; # Define your hostname.
