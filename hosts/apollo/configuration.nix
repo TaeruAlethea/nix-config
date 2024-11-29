073d0fa717faeada2d5da7e9c645ce4b597df591
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-11-29 14:33:01 -0500

    .

--- hosts/apollo/configuration.nix
+++ hosts/apollo/configuration.nix
@@ -20,11 +20,11 @@
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
     
-    (${outputs.nixosSettings} + "/hyprland")
-    (${outputs.nixosSettings} + "/localization")
-    (${outputs.nixosSettings} + "/sops")
-    (${outputs.nixosSettings} + "/stylix")
-    (${outputs.nixosSettings} + "/wayland")
+    ("${outputs.nixosSettings}" + "/hyprland")
+    ("${outputs.nixosSettings}" + "/localization")
+    ("${outputs.nixosSettings}" + "/sops")
+    ("${outputs.nixosSettings}" + "/stylix")
+    ("${outputs.nixosSettings}" + "/wayland")
 
     inputs.sops-nix.nixosModules.sops
     inputs.stylix.nixosModules.stylix
