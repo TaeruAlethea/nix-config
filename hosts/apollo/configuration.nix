bef31a3b81ca22ccaa72f89408910154ab9378f9
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-11-29 18:21:19 -0500

    .

--- hosts/apollo/configuration.nix
+++ hosts/apollo/configuration.nix
@@ -61,16 +61,16 @@
       # Enable flakes and new 'nix' command
       experimental-features = "nix-command flakes";
       # Opinionated: disable global registry
-      #flake-registry = "";
+      flake-registry = "";
       # Workaround for https://github.com/NixOS/nix/issues/9574
-      #nix-path = config.nix.nixPath;
+      nix-path = config.nix.nixPath;
     };
     # Opinionated: disable channels
     channel.enable = false;
 
     # Opinionated: make flake registry and nix path match flake inputs
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+    registry = lib.mkForce lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mkForce lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
   networking.hostName = "apollo"; # Define your hostname.
