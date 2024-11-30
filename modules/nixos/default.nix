536e32193902da7cfc0946573202d3c04ff693a6
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-11-29 19:03:51 -0500

    .

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,11 +1,18 @@
 # Add your reusable NixOS modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
 # These should be stuff you would like to share with others, not your personal configurations.
-{
+{ ... }:{
     # List your module files here
     # my-module = import ./my-module.nix;
 
-    hyprland = import ./hyprland; #todo: make this work right as a module
-    localization = import ./localization;
-    sops = import ./sops;
-    stylix = import ./stylix;
+    imports = [
+        ./hyprland
+        ./localization
+        ./sops
+        ./stylix
+    ];
+
+    #hyprland = import ./hyprland; #todo: make this work right as a module
+    #localization = import ./localization;
+    #sops = import ./sops;
+    #stylix = import ./stylix;
 }
