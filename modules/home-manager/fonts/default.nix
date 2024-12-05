16b2f1b340a05bf49ee14aa057d2fd89f117a1d4
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-12-05 10:52:55 -0500

    Fonts

---
+++ modules/home-manager/fonts/default.nix
@@ -0,0 +1,14 @@
+{ ... }:{
+    fonts.fontconfig = {
+        enable = true;
+        defaultFonts = {
+            emoji = [
+                "Noto Color Emoji"
+                "Noto Emoji"
+            ];
+            monospace = [ "JetBrainsMono NF" ];
+            serif = [ "Noto Serif" ];
+            sansSerif = [ "Noto Sans" ];
+        };
+    };
+}
