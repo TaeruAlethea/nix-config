5331572c4b16a732759de71fc46d468dcf46220c
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-12-14 02:10:38 -0500

    Add WSL Hardware explaination

--- hosts/artemis/hardware-configuration.nix
+++ hosts/artemis/hardware-configuration.nix
@@ -11,6 +11,12 @@
         inputs.nixos-hardware.nixosModules.microsoft-surface-common
     ];
     
+    ######
+    # I consider WSL to be a "hardware-configuration" scope thing.
+    # "hardware" in this context happens to be a Windows container, 
+    # so it counts.
+    ######
+
     # WSL is closer to a container than anything else
     boot.isContainer = true;
 
