7758da2b0b7c2f7216b47229460ca3d3dc7d0858
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-12-14 12:17:37 -0500

    Add NixOS-WSL Reference

--- hosts/artemis/hardware-configuration.nix
+++ hosts/artemis/hardware-configuration.nix
@@ -15,6 +15,7 @@
   # I consider WSL to be a "hardware-configuration" scope thing.
   # "hardware" in this context happens to be a Windows container, 
   # so it counts.
+  # https://github.com/nix-community/NixOS-WSL
   ######
 
   # WSL is closer to a container than anything else
