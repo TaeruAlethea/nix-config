7b9ff84b4bef6b837c0fe3533478de4bbd3b1409
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-12-14 00:40:29 -0500

    .

--- hosts/artemis/configuration.nix
+++ hosts/artemis/configuration.nix
@@ -59,11 +59,9 @@
       #nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
     };
 
-  nixos-wsl.nixosModules.default = {
     wsl.enable = true;
     wsl.defaultUser = "astraeaf";
     wsl.wslConf.network.hostname = "artemis";
-    };
 
   users.mutableUsers = false; # Required for Sops
   # Define a user account. Don't forget to set a password with ‘passwd’.
