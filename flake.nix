a99025f8e537c834ae3178866d66e40f1f31688f
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-12-14 15:19:03 -0500

    .

--- flake.nix
+++ flake.nix
@@ -66,7 +66,7 @@
       # Standalone home-manager configuration entrypoint
       # Available through 'home-manager --flake .#your-username@your-hostname'
       homeConfigurations = {
-        "astraeaf" = home-manager.lib.homeManagerConfiguration {
+        "astraeaf@artermis" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
           extraSpecialArgs = {
             inherit inputs outputs;
