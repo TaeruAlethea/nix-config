6973301b746322449ecb8123557585323f801e0e
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-12-18 15:51:16 -0500

    .

--- modules/home-manager/oh-my-posh/default.nix
+++ modules/home-manager/oh-my-posh/default.nix
@@ -4,6 +4,6 @@
         enable = true;
         enableBashIntegration = true;
         # useTheme = "./uew.omp.json";
-        settings = builtins.fromJSON (builtins.unsafeDiscardStringContext (builtins.readFile "${pkgs.oh-my-posh}/share/oh-my-posh/themes/uew.omp.json"));
+        settings = builtins.fromJSON (builtins.unsafeDiscardStringContext (builtins.readFile "${pkgs.oh-my-posh}/share/oh-my-posh/themes/uew"));
     };
 }
