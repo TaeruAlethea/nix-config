a964a71e6397860e269b5b4ea8b030f86efc1db3
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-09-25 18:48:09 +0000

    re-init Artemis

--- hosts/artemis/configuration.nix
+++ hosts/artemis/configuration.nix
@@ -1,110 +1,54 @@
-{
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
-  ...
-}:
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+
+{ config, lib, pkgs, ... }:
+
 {
   imports = [
-    # If you want to use modules your own flake exports (from modules/nixos):
-    # outputs.nixosModules.example
+    inputs.nixos-wsl.nisosModules.default
 
-    # Or modules from other flakes (such as nixos-hardware):
-    # inputs.hardware.nixosModules.common-cpu-amd
-    # inputs.hardware.nixosModules.common-ssd
-    inputs.nixos-wsl.nixosModules.default
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./users.nix
-    (inputs.self + /modules/nixos/fonts/default.nix)
-    (inputs.self + /modules/nixos/sops/default.nix)
-
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
+    outputs.nixosModules.agenix
+    outputs.nixosModules.fonts
+    outputs.nixosModules.localization
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.stable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
+  wsl ={
+    enable = true;
+    defaultUser = "astraeaf":
+    wslConf = {
+      network.hostname = "artemis";
     };
   };
 
-  nix =
-    let
-      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-    in
-    {
-      settings = {
-        # Enable flakes and new 'nix' command
-        experimental-features = "nix-command flakes";
-        # Opinionated: disable global registry
-        flake-registry = "";
-        # Workaround for https://github.com/NixOS/nix/issues/9574
-        nix-path = config.nix.nixPath;
-      };
-      # Opinionated: disable channels
-      channel.enable = false;
-
-      # Opinionated: make flake registry and nix path match flake inputs
-      #registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-      #nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  nix ={
+    gc.automatic = true;
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
     };
+  };
 
-  wsl.enable = true;
-  wsl.defaultUser = "astraeaf";
-  wsl.wslConf.network.hostname = "artemis";
+  networking.hostname = "artemis";
 
-  environment.etc.hosts.enable = false;
-  environment.etc."resolv.conf".enable = false;
-
-  users.mutableUsers = false; # Required for Sops
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.mutableUsers = false;
   users.users.astraeaf = {
-    description = "Astraea Falke";
     isNormalUser = true;
-    hashedPasswordFile = config.sops.secrets.astraeaf-password.path;
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
+    hashedPasswordFile = config.age.secrets.secret1.path;
+    description = "Astraea Falke";
+    extraGroups = [ "networkmanager" "wheel" ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    git
-
-    # Formatters
-    treefmt2
-    nixfmt-rfc-style
-    yamlfmt
+    helix
   ];
 
-  programs.nh.enable = true;
-
-  environment.sessionVariables = {
-    FLAKE = "/home/astraeaf/nix-config";
+  programs.nh = {
+    enable = true;
+    flake = "/home/astraeaf/nix-config";
   };
 
   # This value determines the NixOS release from which the default
@@ -113,5 +57,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
 }
