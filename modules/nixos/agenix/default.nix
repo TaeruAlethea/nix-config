08f8a90357bcd83fea33aa1d02451ba6d6a4eb81
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-09-25 19:29:44 +0000

    .

--- modules/nixos/agenix/default.nix
+++ modules/nixos/agenix/default.nix
@@ -1,13 +1,12 @@
 { inputs, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    age
     inputs.agenix.packages."${system}".default
   ];
 
 
   age ={
-    identityPaths = [ "/home/astraeaf/.ssh/id_ed25519" ]; # isn't set automatically for some reason
+    identityPaths = [ "~/.ssh/id_ed25519" ]; # isn't set automatically for some reason
 
     secrets.secret1 ={
         file = ( inputs.self + /secrets/secret1.age );
