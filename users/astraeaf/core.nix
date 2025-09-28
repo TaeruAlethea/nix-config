9f0523cf861d932f532b4268ffed4422e88c8dfe
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-09-28 15:43:38 -0400

    Start Stylix refactor

--- users/astraeaf/core.nix
+++ users/astraeaf/core.nix
@@ -11,6 +11,7 @@
 	imports = [
 		outputs.homeManagerModules.helix
 		outputs.homeManagerModules.oh-my-posh
+		outputs.homeManagerModules.stylix
 	];
 
 	nixpkgs.config.allowUnfree = true; # You get used to this, so always have it.
