074dcdbcf828c23e3192d72dba36e6e2b0cdc107
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-10-01 21:43:42 -0400

    formatter pass

--- pkgs/opendeck/default.nix
+++ pkgs/opendeck/default.nix
@@ -1,29 +1,15 @@
-{ stdenv, pkgs, lib, fetchurl }:
-
-stdenv.mkDerivation rec {
+{ appimageTools, fetchurl }:
+let
   pname = "opendeck";
   version = "2.6.1";
-
-  # src = pkgs.fetchFromGitHub {
-  #   owner = "nekename";
-  #   repo = "OpenDeck";
-  #   tag = "v${version}";
-  #   sha256 = "sha256-nI6ZpDdW1pw2FC4XN3q2R1LgYJtu0o47mv8cCDSj0lY=";
-  # };
 
   src = fetchurl {
     url = "https://github.com/nekename/OpenDeck/releases/download/v${version}/opendeck_${version}_amd64.AppImage";
     sha256 = "sha256:e12f0330d2a6349f8be702b94dc6839af33c10e04a5e0b86641ad6fd10959cd8";
   };
-
-  dontBuild = true;
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp ${pname} $out/bin/
-  '';
-
-  #propagatedBuildInputs = [ runTimePackage ];
+in
+appimageTools.wrapType2 rec {
+  inherit pname version src;
 
   meta = {
     homepage = "https://github.com/nekename/OpenDeck";
