53c75636d1f1469e24282a7a29ea6cafca443355
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-10-10 16:38:52 -0400

    .

--- modules/nixos/fonts/default.nix
+++ modules/nixos/fonts/default.nix
@@ -1,9 +1,10 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 {
   fonts = {
     packages = with pkgs; [
       # Fallback Fonts
       nerd-fonts.jetbrains-mono
+      nerd-fonts.symbols-only
 
       noto-fonts
       noto-fonts-cjk-sans
