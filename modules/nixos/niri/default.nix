53704a6aa12b43a4bf66bca277207793fefc476c
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-10-13 16:07:33 -0400

    More Niri

--- modules/nixos/niri/default.nix
+++ modules/nixos/niri/default.nix
@@ -1,5 +1,19 @@
-{ pkgs, ... } :
+{ inputs, pkgs, ... } :
 {
+	nixpkgs = {
+		overlays = [
+			inputs.niri.overlays.niri
+			# Memory leak in mesa. Last known good version is 25.1.7
+			# # https://gitlab.freedesktop.org/mesa/mesa/-/issues/13780
+			(final: prev: {
+				mesa =
+					(builtins.getFlake "github:NixOS/nixpkgs/7c7fa70f697b01bb00fa9cc1132c51315836ad5c")
+					.legacyPackages.${pkgs.system}.mesa;
+			})
+		];
+	};
+
+
 	programs.niri = {
 	  enable = true;
 	  package = pkgs.niri-unstable;
