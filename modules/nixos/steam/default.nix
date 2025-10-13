057f3b55b76eae5b395a45013b9e7fbb86b93691
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-10-13 18:41:09 -0400

    .

--- modules/nixos/steam/default.nix
+++ modules/nixos/steam/default.nix
@@ -1,11 +1,15 @@
 { ... }:
 {
+	programs.gamescope.enable = true;
+	
 	programs.steam = {
 		enable = true;
 		extest.enable = true;
+		gamescopeSession = true;
 		
 		remotePlay.openFirewall = true;
 		dedicatedServer.openFirewall = true;
 		localNetworkGameTransfers.openFirewall = true;
 	};
 }
+ 
