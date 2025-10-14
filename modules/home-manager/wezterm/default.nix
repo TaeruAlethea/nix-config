8f34f9b324dc55e10943fa0fc2241486b8ee47c4
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-10-14 00:03:20 -0400

    .

--- modules/home-manager/wezterm/default.nix
+++ modules/home-manager/wezterm/default.nix
@@ -3,12 +3,14 @@
   programs.wezterm = {
     enable = true;
     enableBashIntegration = true;
-    extraConfig = "return{
-                font = wezterm.font 'JetBrains Mono',
-                font_size = 12,
-                hide_mouse_cursor_when_typing = false,
-                initial_cols = 120,
-                initial_rows = 28
-            }";
+    extraConfig =
+      "return{
+        font = wezterm.font 'JetBrains Mono',
+        font_size = 12,
+        hide_mouse_cursor_when_typing = false,
+        initial_cols = 120,
+        initial_rows = 28
+        window_background_opacity = 0.7
+      }";
   };
 }
