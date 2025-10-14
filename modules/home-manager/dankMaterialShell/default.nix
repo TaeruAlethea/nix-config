775cdbad39d43e1fc37e5115aba7d539037109b7
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-10-13 21:26:00 -0400

    I want clipboard history

--- modules/home-manager/dankMaterialShell/default.nix
+++ modules/home-manager/dankMaterialShell/default.nix
@@ -1,8 +1,12 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
 	imports = [
 		inputs.dankMaterialShell.homeModules.dankMaterialShell.default
 		inputs.dankMaterialShell.homeModules.dankMaterialShell.niri
+	];
+
+	home.packages = with pkgs; [
+		wl-clipboard
 	];
 
 	programs.dankMaterialShell.enable = true;
