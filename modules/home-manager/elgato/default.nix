fb0537f4752d07f53199e913602490b73652e989
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-10-23 21:09:12 -0400

    Elgato audio volume fix?

--- modules/home-manager/elgato/default.nix
+++ modules/home-manager/elgato/default.nix
@@ -1,4 +1,4 @@
 { ... }: {
 	xdg.configFile."wireplumber/wireplumber.conf.d/51-wavexlr.conf".source = ./51-wavexlr.conf;
-	xdg.configFile."wireplumber/scripts/wavexlrfix.lua".source = ./wavexlrfix.lua;
+	xdg.dataFile."wireplumber/scripts/wavexlrfix.lua".source = ./wavexlrfix.lua;
 }
