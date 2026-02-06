5751f496099e950ab8ba03aa8b3fccbce25ce375
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-02-05 22:12:05 -0500

    .

--- modules/home-manager/niri/default.nix
+++ modules/home-manager/niri/default.nix
@@ -87,7 +87,7 @@
 			#};
 
 			binds = {
-				"Mod+Shift+Slash".action.show-hotkey-overlay = true;
+				"Mod+Shift+Slash".action.show-hotkey-overlay = [];
 
 				"Mod+T" = {
 					hotkey-overlay.title = "Open a Terminal: wezterm";
@@ -119,59 +119,59 @@
 				# 	action.spawn = ["dms" "ipc" "call" "brightness" "decrement" "5" ""];
 				# };
 
-				"Mod+Tab" = { repeat = false; action.toggle-overview = true; };
-				"Mod+Q" = { repeat = false; action.close-window = true; };
+				"Mod+Tab" = { repeat = false; action.toggle-overview = []; };
+				"Mod+Q" = { repeat = false; action.close-window = []; };
 
-				"Mod+Left"  = { action.focus-column-left = true; };
-				"Mod+Down"  = { action.focus-window-down = true; };
-				"Mod+Up"    = { action.focus-window-up = true; };
-				"Mod+Right" = { action.focus-column-right = true; };
-				"Mod+H"     = { action.focus-column-left = true; };
-				"Mod+J"     = { action.focus-window-down = true; };
-				"Mod+K"     = { action.focus-window-up = true; };
-				"Mod+L"     = { action.focus-column-right = true; };
+				"Mod+Left"  = { action.focus-column-left = []; };
+				"Mod+Down"  = { action.focus-window-down = []; };
+				"Mod+Up"    = { action.focus-window-up = []; };
+				"Mod+Right" = { action.focus-column-right = []; };
+				"Mod+H"     = { action.focus-column-left = []; };
+				"Mod+J"     = { action.focus-window-down = []; };
+				"Mod+K"     = { action.focus-window-up = []; };
+				"Mod+L"     = { action.focus-column-right = []; };
 
-				"Mod+Ctrl+Left"  = { action.move-column-left = true; };
-				"Mod+Ctrl+Down"  = { action.move-window-down = true; };
-				"Mod+Ctrl+Up"    = { action.move-window-up = true; };
-				"Mod+Ctrl+Right" = { action.move-column-right = true; };
-				"Mod+Ctrl+H"     = { action.move-column-left = true; };
-				"Mod+Ctrl+J"     = { action.move-window-down = true; };
-				"Mod+Ctrl+K"     = { action.move-window-up = true; };
-				"Mod+Ctrl+L"     = { action.move-column-right = true; };
+				"Mod+Ctrl+Left"  = { action.move-column-left = []; };
+				"Mod+Ctrl+Down"  = { action.move-window-down = []; };
+				"Mod+Ctrl+Up"    = { action.move-window-up = []; };
+				"Mod+Ctrl+Right" = { action.move-column-right = []; };
+				"Mod+Ctrl+H"     = { action.move-column-left = []; };
+				"Mod+Ctrl+J"     = { action.move-window-down = []; };
+				"Mod+Ctrl+K"     = { action.move-window-up = []; };
+				"Mod+Ctrl+L"     = { action.move-column-right = []; };
 
-				"Mod+Shift+Left"  = { action.focus-monitor-left = true; };
-				"Mod+Shift+Down"  = { action.focus-monitor-down = true; };
-				"Mod+Shift+Up"    = { action.focus-monitor-up = true; };
-				"Mod+Shift+Right" = { action.focus-monitor-right = true; };
-				"Mod+Shift+H"     = { action.focus-monitor-left = true; };
-				"Mod+Shift+J"     = { action.focus-monitor-down = true; };
-				"Mod+Shift+K"     = { action.focus-monitor-up = true; };
-				"Mod+Shift+L"     = { action.focus-monitor-right = true; };
+				"Mod+Shift+Left"  = { action.focus-monitor-left = []; };
+				"Mod+Shift+Down"  = { action.focus-monitor-down = []; };
+				"Mod+Shift+Up"    = { action.focus-monitor-up = []; };
+				"Mod+Shift+Right" = { action.focus-monitor-right = []; };
+				"Mod+Shift+H"     = { action.focus-monitor-left = []; };
+				"Mod+Shift+J"     = { action.focus-monitor-down = []; };
+				"Mod+Shift+K"     = { action.focus-monitor-up = []; };
+				"Mod+Shift+L"     = { action.focus-monitor-right = []; };
 
-				"Mod+Shift+Ctrl+Left"  = { action.move-column-to-monitor-left = true; };
-				"Mod+Shift+Ctrl+Down"  = { action.move-column-to-monitor-down = true; };
-				"Mod+Shift+Ctrl+Up"    = { action.move-column-to-monitor-up = true; };
-				"Mod+Shift+Ctrl+Right" = { action.move-column-to-monitor-right = true; };
-				"Mod+Shift+Ctrl+H"     = { action.move-column-to-monitor-left = true; };
-				"Mod+Shift+Ctrl+J"     = { action.move-column-to-monitor-down = true; };
-				"Mod+Shift+Ctrl+K"     = { action.move-column-to-monitor-up = true; };
-				"Mod+Shift+Ctrl+L"     = { action.move-column-to-monitor-right = true; };
+				"Mod+Shift+Ctrl+Left"  = { action.move-column-to-monitor-left = []; };
+				"Mod+Shift+Ctrl+Down"  = { action.move-column-to-monitor-down = []; };
+				"Mod+Shift+Ctrl+Up"    = { action.move-column-to-monitor-up = []; };
+				"Mod+Shift+Ctrl+Right" = { action.move-column-to-monitor-right = []; };
+				"Mod+Shift+Ctrl+H"     = { action.move-column-to-monitor-left = []; };
+				"Mod+Shift+Ctrl+J"     = { action.move-column-to-monitor-down = []; };
+				"Mod+Shift+Ctrl+K"     = { action.move-column-to-monitor-up = []; };
+				"Mod+Shift+Ctrl+L"     = { action.move-column-to-monitor-right = []; };
 
 				# "Mod+WheelScrollDown"      = { cooldown-ms = 150; action.focus-workspace-down = true; };
 				# "Mod+WheelScrollUp"        = { cooldown-ms = 150; action.focus-workspace-up = true; };
 				# "Mod+Ctrl+WheelScrollDown" = { cooldown-ms = 150; action.move-column-to-workspace-down = true; };
 				# "Mod+Ctrl+WheelScrollUp"   = { cooldown-ms = 150; move-column-to-workspace-up = true; };
 				
-				"Mod+WheelScrollRight"      = { action.focus-column-right = true; };
-				"Mod+WheelScrollLeft"       = { action.focus-column-left = true; };
-				"Mod+Ctrl+WheelScrollRight" = { action.move-column-right = true; };
-				"Mod+Ctrl+WheelScrollLeft"  = { action.move-column-left = true; };
+				"Mod+WheelScrollRight"      = { action.focus-column-right = []; };
+				"Mod+WheelScrollLeft"       = { action.focus-column-left = []; };
+				"Mod+Ctrl+WheelScrollRight" = { action.move-column-right = []; };
+				"Mod+Ctrl+WheelScrollLeft"  = { action.move-column-left = []; };
 
 				# "Mod+Shift+WheelScrollDown"      = { action.focus-column-right = true; };
 				# "Mod+Shift+WheelScrollUp"        = { action.focus-column-left = true; };
-				"Mod+Ctrl+Shift+WheelScrollDown" = { action.move-column-right = true; };
-				"Mod+Ctrl+Shift+WheelScrollUp"   = { action.move-column-left = true; };
+				"Mod+Ctrl+Shift+WheelScrollDown" = { action.move-column-right = []; };
+				"Mod+Ctrl+Shift+WheelScrollUp"   = { action.move-column-left = []; };
 
 				"Mod+1" = { action.focus-workspace = 1; };
 				"Mod+2" = { action.focus-workspace = 2; };
@@ -193,35 +193,35 @@
 				"Mod+Ctrl+8" = { action.move-column-to-workspace = 8; };
 				"Mod+Ctrl+9" = { action.move-column-to-workspace = 9; };
 
-				"Mod+BracketLeft"  = { action.consume-or-expel-window-left = true; };
-				"Mod+BracketRight" = { action.consume-or-expel-window-right = true; };
+				"Mod+BracketLeft"  = { action.consume-or-expel-window-left = []; };
+				"Mod+BracketRight" = { action.consume-or-expel-window-right = []; };
 
-				"Mod+R"            = { action.switch-preset-column-width = true; };
-				"Mod+Shift+R"      = { action.switch-preset-window-height = true; };
-				"Mod+Ctrl+R"       = { action.reset-window-height = true; };
-				"Mod+F"            = { action.maximize-column = true; };
-				"Mod+Shift+F"      = { action.fullscreen-window = true; };
-				"Mod+Ctrl+F"       = { action.expand-column-to-available-width = true; };
-				"Mod+Shift+C"      = { action.center-column = true; };
-				"Mod+Ctrl+C"       = { action.center-visible-columns = true; };
+				"Mod+R"            = { action.switch-preset-column-width = []; };
+				"Mod+Shift+R"      = { action.switch-preset-window-height = []; };
+				"Mod+Ctrl+R"       = { action.reset-window-height = []; };
+				"Mod+F"            = { action.maximize-column = []; };
+				"Mod+Shift+F"      = { action.fullscreen-window = []; };
+				"Mod+Ctrl+F"       = { action.expand-column-to-available-width = []; };
+				"Mod+Shift+C"      = { action.center-column = []; };
+				"Mod+Ctrl+C"       = { action.center-visible-columns = []; };
 
 				"Mod+Minus"       = { action.set-column-width = "-10%"; };
 				"Mod+Equal"       = { action.set-column-width = "+10%"; };
 				"Mod+Shift+Minus" = { action.set-window-height = "-10%"; };
 				"Mod+Shift+Equal" = { action.set-window-height = "+10%"; };
-				"Mod+Shift+V"     = { action.toggle-window-floating = true; };
+				"Mod+Shift+V"     = { action.toggle-window-floating = []; };
 
-				"Mod+W"       = { action.toggle-column-tabbed-display = true; };
+				"Mod+W"       = { action.toggle-column-tabbed-display = []; };
 
 				"Mod+Shift+S" = { action.screenshot = { show-pointer = false; }; };
 				"Print"       = { action.screenshot = { show-pointer = false; }; };
 				"Ctrl+Print"  = { action.screenshot-screen = { show-pointer = false; }; };
-				"Alt+Print"   = { action.screenshot-window = { show-pointer = false; }; };
+				"Alt+Print"   = { action.screenshot-window = []; };
 
-				"Mod+Shift+E"     = { action.quit = true; };
-				"Ctrl+Alt+Delete" = { action.quit = true; };
+				"Mod+Shift+E"     = { action.quit = []; };
+				"Ctrl+Alt+Delete" = { action.quit = []; };
 
-				"Mod+Shift+P" = { action.power-off-monitors = true; };
+				"Mod+Shift+P" = { action.power-off-monitors = []; };
 
 				# "Mod+Space" = {
 				# 	hotkey-overlay-title = "Application Launcher";
