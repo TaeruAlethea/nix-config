c6503fd1dac0bb8c16bd3d4d06f4ff8577850682
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-02-08 15:29:38 -0500

    lol lmao

--- modules/hosts/default.nix
+++ modules/hosts/default.nix
@@ -10,26 +10,34 @@
         
         self.nixosModules.defaultFonts
 
+        inputs.home-manager.lib.homeManagerConfiguration {
+              pkgs = import inputs.nixpkgs {
+                config.allowUnfree = true;
+                system = "x86_64-linux";
+              };
+              modules = [ 
+                ./artemis/home.nix
+                #./astraeaf/artemis.nix
+
+                #self.homeModules.oh-my-posh
+                #self.homeModules.helix
+              ];
+            }
+
         inputs.home-manager.nixosModules.home-manager 
         {
-          homeConfigurations.astraeaf = inputs.home-manager.lib.homeManagerConfiguration {
-            pkgs = import inputs.nixpkgs {
-              config.allowUnfree = true;
-              system = "x86_64-linux";
-            };
-            modules = [ 
-              ./artemis/home.nix
-
-              self.homeModules.oh-my-posh
-              #self.homeModules.helix
-            ];
-          };
-
           home-manager = {
             useGlobalPkgs = true;
             useUserPackages = true;
+            users.astraeaf = ./artemis/home.nix;
             backupFileExtension = "bk";
             overwriteBackup = true;
+            home-manager.extraSpecialArgs = {
+          
+              # bring in the list of inputs into the home-manager module
+              inherit inputs;
+              system = "x86_64-linux";
+            };
           };
         }
       ];
