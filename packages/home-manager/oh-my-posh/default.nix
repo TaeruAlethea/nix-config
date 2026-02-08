a9eaa4f51973a31d8a46c1b953042c5aa1aea9ce
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-02-08 00:10:10 -0500

    Package helix and Oh-my-posh

--- packages/home-manager/oh-my-posh/default.nix
+++ packages/home-manager/oh-my-posh/default.nix
@@ -1,12 +1,10 @@
-{ lib, inputs, ... }:
-let
-  configPath = (inputs.self + "/modules/home-manager/oh-my-posh/foxxo_dark.omp.json");
-in
-{
-  programs.oh-my-posh = {
-    enable = true;
-    enableBashIntegration = true;
-    settings = builtins.fromJSON (builtins.unsafeDiscardStringContext (builtins.readFile configPath));
-    #useTheme = "sonicboom_dark";
+{ ... }: {
+  perSystem = { lib, ... }: {
+    programs.oh-my-posh = {
+      enable = true;
+      enableBashIntegration = true;
+      settings = builtins.fromJSON (builtins.unsafeDiscardStringContext (builtins.readFile ./foxxo_dark.omp.json));
+      #useTheme = "sonicboom_dark";
+    };
   };
 }
