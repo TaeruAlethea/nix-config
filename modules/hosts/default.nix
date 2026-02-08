a9f6e2af74132fbdbcaf8f18a3674a60bab3a0c6
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-02-08 13:53:36 -0500

    .

--- modules/hosts/default.nix
+++ modules/hosts/default.nix
@@ -10,7 +10,7 @@
         
         self.nixosModules.defaultFonts
 
-        self.home-manager.nixosModules.home-manager 
+        inputs.home-manager.nixosModules.home-manager 
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
