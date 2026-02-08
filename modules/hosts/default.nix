ff223cb1c7b0e35bae8cb17eb295be7dd1d64183
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-02-07 20:54:08 -0500

    .

--- modules/hosts/default.nix
+++ modules/hosts/default.nix
@@ -1,11 +1,14 @@
-{ inputs, ...}: {
+{ inputs, self, ...}: {
   flake = {
     nixosConfigurations.ares = inputs.nixpkgs.lib.nixosSystem {
       modules = [ ./ares ];
     };
 
     nixosConfigurations.artemis = inputs.nixpkgs.lib.nixosSystem {
-      modules = [ ./artemis ];
+      modules = [ 
+        ./artemis
+        self.flake.nixosModules.defaultFonts
+      ];
     };
 
     nixosConfigurations.zeus = inputs.nixpkgs.lib.nixosSystem {
