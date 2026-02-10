bb27a6779cd7ac301fb90116c13420b36241ff59
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-02-10 15:36:33 -0500

    needed using

--- packages/nixos/niri/default.nix
+++ packages/nixos/niri/default.nix
@@ -1,5 +1,5 @@
 { ... }: {
-	flake.nixosModules.niri = { pkgs, ... } :
+	flake.nixosModules.niri = { pkgs, inputs, ... } :
 	{
 		nixpkgs.overlays = [ inputs.niri.overlays.niri ];
 		programs.niri.package = pkgs.niri-unstable;
