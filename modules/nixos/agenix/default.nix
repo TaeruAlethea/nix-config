fcf01ee11385c7ffc7248435e3210369030a7474
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-03-03 16:05:39 -0500

    .

--- modules/nixos/agenix/default.nix
+++ modules/nixos/agenix/default.nix
@@ -13,7 +13,7 @@
       identityPaths = [ "/home/astraeaf/.ssh/id_ed25519" ]; # isn't set automatically for some reason
 
       secrets.secret1 = {
-        rekeyFile = ./secrets/secret1.age;
+        rekeyFile = ./secrets/secret2.age;
         owner = "astraeaf";
       };
 
