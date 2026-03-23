c43ebf3da0800380d5b1a7b31f35335f684fe350
Astraea Falke <61478538+TaeruAlethea@users.noreply.github.com>
2026-03-23 14:04:02 -0400

    not using this

--- modules/nixos/sops/default.nix
+++ modules/nixos/sops/default.nix
@@ -8,7 +8,6 @@
 		sops = {
 			age = {
 				sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-				keyFile = "/var/lib/sops-nix/key.txt";
 			};
 			defaultSopsFile = ./secrets/secrets.yaml;
 			secrets = {
