60e3dfd27f31f5772f5219ebbc9ecb870720a969
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-03-23 13:02:01 -0400

    add secrets file

--- modules/nixos/sops/default.nix
+++ modules/nixos/sops/default.nix
@@ -10,7 +10,7 @@
 				sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 				keyFile = "/var/lib/sops-nix/key.txt";
 			};
-
+			defaultSopsFile = ./secrets.yaml;
 			secrets = {
 				"astraeaf-pw" = {};
 			};
