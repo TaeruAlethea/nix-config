2e7d654918880e7be1cf4a4afdba55e596f49de3
Astraea Falke <61478538+TaeruAlethea@users.noreply.github.com>
2026-04-15 11:18:39 -0400

    .

---
+++ modules/inputs.nix
@@ -0,0 +1,48 @@
+{
+	flake-file.inputs = {
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+
+    agenix.url = "github:ryantm/agenix";
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dankMaterialShell = {
+      url = "github:AvengeMedia/DankMaterialShell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dms-plugin-registry = {
+      url = "github:AvengeMedia/dms-plugin-registry";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    matugen = {
+      url = "github:/InioX/Matugen";
+      inputs.nixpkgs.follows = "nixpkgs";
+      # If you need a specific version:
+      # ref = "refs/tags/matugen-v0.10.0";
+    };
+
+    stylix.url = "github:danth/stylix";
+
+    # Only needed for Windows Subsystem for Linux
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    nix-doom-emacs-unstraightened.url = "github:marienz/nix-doom-emacs-unstraightened";
+
+    firefox-addons.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+
+    nixcord.url = "github:FlameFlag/nixcord";
+  };
+}
