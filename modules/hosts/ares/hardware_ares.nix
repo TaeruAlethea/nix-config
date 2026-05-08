8998e3854f088b3caa2516bfb91a21e01bd4f99e
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-05-08 13:38:37 -0400

    smh

--- modules/hosts/ares/hardware_ares.nix
+++ modules/hosts/ares/hardware_ares.nix
@@ -54,7 +54,7 @@ flake.modules.nixos.hardware_ares = {
 
   # Surface SP5 Specific Configs
   services.thermald.enable = true;
-  mircrosoft-surface.ipts.enable = true;
+  microsoft-surface.ipts.enable = true;
     # microsoft-surface.surface-control.enable = true; # Still Inop
   };
 }
