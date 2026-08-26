38b0bf02f28cb8ee9d0a82886fe344b4f483b83e
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-08-26 16:07:13 -0400

    Modularize Hera

--- modules/hosts/hera/configuration.nix
+++ modules/hosts/hera/configuration.nix
@@ -1,4 +1,7 @@
-# Edit this configuration file to define what should be installed on
+
+{ inputs, ... }:
+{
+  flake.modules.nixos.system_hera = # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
@@ -120,5 +123,5 @@
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "26.05"; # Did you read the comment?
 
+};
 }
-
