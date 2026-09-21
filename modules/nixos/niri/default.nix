6aa1e0a698d05bf6c2dd97e19969db3c36c67bf3
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-09-21 19:34:13 -0400

    update DMS and Trying to fix Nautilus thumbs

--- modules/nixos/niri/default.nix
+++ modules/nixos/niri/default.nix
@@ -12,7 +12,20 @@
       #   inputs.niri.nixosModules.niri
       # ];
 
-      nixpkgs.overlays = [ inputs.niri.overlays.niri ];
+      nixpkgs.overlays = [
+        inputs.niri.overlays.niri
+        (final: prev: {
+          nautilus = prev.nautilus.overrideAttrs (nprev: {
+            buildInputs =
+              nprev.buildInputs
+              ++ (with pkgs.gst_all_1; [
+                gst-plugins-good
+                gst-plugins-bad
+              ]);
+          });
+        })
+      ];
+      
       programs.niri.package = pkgs.niri-unstable;
 
       systemd.user.services.niri-flake-polkit.enable = false;
